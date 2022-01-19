b71285c42bf0a87f65efe5c0b4854f5f559a9c45
AsirAAlam <46213673+AsirAAlam@users.noreply.github.com>
2022-01-19 10:52:35 -0500

    Created AddUser2.php, which prevents duplicate usernames.

---
+++ api/AddUser2.php
@@ -0,0 +1,56 @@
+<?php
+	$inData = getRequestInfo();
+
+	$first_name = $inData["first_name"];
+	$last_name = $inData["last_name"];
+	$phone = $inData["phone"];
+	$username = $inData["username"];
+	$password = $inData["password"];
+
+	$conn = new mysqli("localhost", "lampy", "P@ssw0rd", "lamp");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+    $srch = $conn->prepare("SELECT * FROM users WHERE username=?");
+		$srch->bind_param("s", $username);
+		$srch->execute();
+		$result = $srch->get_result();
+
+    if ($result->fetch_assoc())
+    {
+      returnWithError("The username already exists.");
+    }
+    else
+    {
+      $stmt = $conn->prepare("INSERT into users (first_name, last_name, phone, username, password) VALUES(?,?,?,?,?)");
+      $stmt->bind_param("ssiss", $first_name, $last_name, $phone, $username, $password);
+      $stmt->execute();
+      $stmt->close();
+      returnWithError("User added.");
+    }
+    
+    $srch->close();
+    $conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
