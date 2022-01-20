2833120bc6b495b47099b97b4eb42b2b838bd10d
AsirAAlam <46213673+AsirAAlam@users.noreply.github.com>
2022-01-20 15:57:42 -0500

    Updated AddContact.php -> works now

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,23 +1,25 @@
 <?php
 	$inData = getRequestInfo();
 
+
+  $user_id = $inData["user_id"];
 	$first_name = $inData["first_name"];
 	$last_name = $inData["last_name"];
-	$phone = $inData["phone"]
+	$phone = $inData["phone"];
 
-	$conn = new mysqli("localhost", "lampy", "password123", "lamp");
+	$conn = new mysqli("localhost", "lampy", "P@ssw0rd", "lamp");
 	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
 	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into contacts (first_name, last_name, phone) VALUES(?,?,?)");
-		$stmt->bind_param("ssi", $first_name, $last_name, $phone);
+		$stmt = $conn->prepare("INSERT into contacts (user_id, first_name, last_name, phone) VALUES(?,?,?,?)");
+		$stmt->bind_param("isss", $user_id, $first_name, $last_name, $phone);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
-		returnWithError("");
+		returnWithError("25");
 	}
 
 	function getRequestInfo()
