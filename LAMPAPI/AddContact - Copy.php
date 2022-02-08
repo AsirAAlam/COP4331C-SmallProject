ff9b40f303dd2a39922a8409108adfd815df110e
AsirAAlam <46213673+AsirAAlam@users.noreply.github.com>
2022-02-07 19:39:20 -0500

    AddContact Update
    Created temp backup
    Added email info

--- LAMPAPI/AddContact - Copy.php
+++ LAMPAPI/AddContact - Copy.php
@@ -5,7 +5,6 @@
 	$first_name = $inData["first_name"];
 	$last_name = $inData["last_name"];
 	$phone = $inData["phone"];
-	$email = $inData["email"];
 
 	$conn = new mysqli("localhost", "lampy", "P@ssw0rd", "lamp");
 	if ($conn->connect_error)
@@ -14,8 +13,8 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into contacts (user_id, first_name, last_name, phone, email) VALUES(?,?,?,?,?)");
-		$stmt->bind_param("issss", $user_id, $first_name, $last_name, $phone, $email);
+		$stmt = $conn->prepare("INSERT into contacts (user_id, first_name, last_name, phone) VALUES(?,?,?,?)");
+		$stmt->bind_param("isss", $user_id, $first_name, $last_name, $phone);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
