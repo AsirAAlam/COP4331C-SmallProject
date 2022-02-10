182bb99e66d3d263876e08261aac28cc201d075d
Matthew McKeever <51388006+MatthewMcK17@users.noreply.github.com>
2022-02-10 14:23:48 -0500

    Added security

--- php/SearchContact.php
+++ php/SearchContact.php
@@ -1,10 +1,11 @@
 <?php
+	require_once("LoadEnv.php");
 	$inData = getRequestInfo();
 
 	$user_id = $inData["user_id"];
 	$name = $inData["name"];
 
-	$conn = new mysqli("localhost", "lampy", "P@ssw0rd", "lamp");
+	$conn = (new LoadEnv(__DIR__.'/../../.env', __DIR__.'/../../.key'))->load();
 	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
@@ -12,15 +13,15 @@
 	else
 	{
 		$stmt = $conn->prepare("
-      SELECT * FROM contacts 
-      WHERE user_id=$user_id
-      AND (first_name LIKE '%$name%'
-      OR last_name LIKE '%$name%'
-      OR CONCAT(first_name, ' ', last_name) LIKE '%$name%'
-      OR email LIKE '%$name%'
-      OR phone LIKE '%$name%')
-      ORDER BY first_name, last_name, phone, email, user_id, contact_id;
-    ");
+     			SELECT * FROM contacts 
+      			WHERE user_id=$user_id
+      			AND (first_name LIKE '%$name%'
+     			OR last_name LIKE '%$name%'
+      			OR CONCAT(first_name, ' ', last_name) LIKE '%$name%'
+      			OR email LIKE '%$name%'
+      			OR phone LIKE '%$name%')
+      			ORDER BY first_name, last_name, phone, email, user_id, contact_id;
+    			");
 		$stmt->execute();
 		$result = $stmt->get_result();
 
@@ -31,7 +32,7 @@
 			$rows[] = $row;
 		}
 
-    sendResultInfoAsJson(json_encode( $rows ));
+    		sendResultInfoAsJson(json_encode( $rows ));
 
 		$stmt->close();
 		$conn->close();
